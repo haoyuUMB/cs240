361b7dc7911577851a63333804192f9357b97a4a
Haoyu <haoyuumb@gmail.com>
2022-09-13 23:47:25 -0400

    lec3

---
+++ lec3/main2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	unsigned int a = 1000;
+	signed int b = -1;
+	if ((signed int) a > b)
+		printf("a is more than b");
+	else
+		printf("a is less or equal than b");
+	return 0;
+}
