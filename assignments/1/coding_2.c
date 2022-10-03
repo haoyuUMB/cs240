6513efe92cead66268c73e0f63070944bc9fb4e7
Haoyu <haoyuumb@gmail.com>
2022-10-02 23:45:57 -0400

    assignment1

---
+++ assignments/1/coding_2.c
@@ -0,0 +1,57 @@
+//
+// Created by haoyu on 9/18/2022.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct string {
+    char *s;
+    int len, cap;
+} String;
+
+
+String *append_string(String *s, char c) {
+    if (s->cap == s->len) {
+        s->cap <<= 1;
+        s->s = (char *) realloc(s->s, s->cap);
+    }
+    s->s[s->len++] = c;
+    s->s[s->len] = 0;
+    return s;
+}
+
+char *getLine(String *s) {
+    s->len = 0;
+    char c;
+    while (1) {
+        c = getchar();
+        if (c == -1 || c == '\n') return s->s;
+        append_string(s, c);
+    }
+}
+
+
+int solution(char *S, char *F) {
+    /*Complete this function*/
+}
+
+int main(int argc, char *argv[]) {
+    int t;
+    String s1 = {malloc(10), 0, 10},
+            s2 = {malloc(10), 0, 10};
+    t = atoi(getLine(&s1));
+    for (int i = 1; i <= t; ++i) {
+        getLine(&s1);
+        getLine(&s2);
+
+        if (argc == 1) {
+            printf("Case #%d: %d\n", i, solution(s1.s, s2.s));
+        } else if (atoi(argv[1]) == i) {
+            printf("s1: %s\n", s1.s);
+            printf("s2: %s\n", s2.s);
+        }
+        s1.len = 0;
+        s2.len = 0;
+    }
+    return 0;
+}
