332482f8cb1c9edb730ea6a2341e31ef7cb2122e
haoyuUMB <haoyu.wang001@umb.edu>
2022-10-11 10:34:25 -0400

    assignment2

--- lib_demo/client.c
+++ lib_demo/client.c
@@ -1,6 +1,19 @@
 
 #include <utils.h>
 
+
 int main() {
     print_utils();
+    char *ptr[3];
+    ptr[0] = malloc(sizeof(char) * 10);
+    ptr[0] = "hello1";
+    printf(ptr[0]);
+    char **lineptr1;
+    lineptr1 = malloc(sizeof(char *) * 3);
+    lineptr1[0] = malloc(sizeof(char) * 10);
+    lineptr1[0] = "hello";
+    printf(lineptr1[0]);
+
+    lineptr1 = ptr;
+    printf(lineptr1[0]);
 }
