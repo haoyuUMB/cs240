b9f4b2a91ffadcf91b21ab8826bf204949d176c3
Haoyu <haoyuumb@gmail.com>
2023-01-26 19:33:31 -0500

    lec2

---
+++ src/lec2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+void add(int a, int  b){};
+void subtraction(int a, int  b){return ;}
+
+int main(){
+    printf("hello world");
+
+    //
+    int a1_, A1, a, A, _a = 4, hellWorld;
+}
