bc1deaa4519bdc38d3efe828c085c51b1ffc08b4
haoyu <haoyu.wang001@umb.edu>
2023-02-02 18:58:51 -0500

    lec3 continued

--- src/lec3/main.c
+++ src/lec3/main.c
@@ -1,17 +1,85 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define MY_CHAR_MAX 25
+
 //What will be printed out?
 //# printf is a formatted print function
 //# the % is a placeholder for displaying a data type as a string
 int main(void)
 {
-    char a = CHAR_MAX, b = CHAR_MAX + 1;
-    printf("max char: %d\n", a);
-    printf("max char + 1:%d", b);
-    printf("char if 65 + 1:%c\n", 65);
+    // uninitialized variables have wild values
+    int a = 4 , b;
+    printf("%d\n", b);
 
-    printf("address of a: %p, size of a: %u", &a, sizeof(a));
+    // p stands for pointer used in printf for printing address
+    // u means unsigned integer
+    printf("the address of b is: 0x%p, the size is: %u bytes\n", &b, sizeof(b));
+
+
+    signed int c; // <==> int c; there is 1 highest bit for sign bit 1 stands negative and 0 stands postive
+    unsigned int d; // there is no sign bit
+    // as a result: signed integer has 31 bits of values bits and unsign has 32 bits of values
+    // 0000 0000  0000 0000 0000 0000 0000 0000 0000 0000
+
+
+    char ccc = 1 + 1;
+    int ddd = ccc + 22;
+    printf("max char: %d\n", CHAR_MAX);
+    printf("min char: %d\n", CHAR_MIN);
+
+    printf("unsigned char max char: %d\n", UCHAR_MAX);
+
+
+    printf("my char max: %d\n", MY_CHAR_MAX);
+
+
+    char c1 = CHAR_MAX + 1;
+
+    // 0111 1111  CHAR_MAX (1byte)
+    // 1111 1111  CHAR_MIN (1byte)
+
+    // 1111 1111 UCHAR_MAX
+    // 0000 0000 UCHAR_MIN
+
+
+    // 0111 1111  CHAR_MAX (1byte) + 1  => 1111 1111 (CHAR_MIN)
+
+    printf("c1 char: %d\n", c1);
+    printf("c1 char: %d\n", CHAR_MIN);
+
+    // 1111 1111  CHAR_MIN (1byte) - 1 => 1111 1110
+
+    // -1
+    // 1000 0001 flip every bit except the sign bit 1's com
+    // 1111 1110 + 1 2'com
+    // 1111 1111
+
+    // 1111 1111 - 1
+    // 1111 1111  CHAR_MIN
+    // 0111 1111
+
+    char c2 = CHAR_MIN - 1;  // int - int
+    printf("c2 char: %d\n", c2); // next class
+
+
+    // -1 char
+
+    char c3 = -1;
+    printf("unsigned char of signed char -1: %x\n", c3);
+
+    // 15  <==> 1111   8+4+2+1 = 15  f
+
+
+
+
+
+//    char aa = CHAR_MAX, bb = CHAR_MAX + 1;
+//    printf("max char: %d\n", aa);
+//    printf("max char + 1:%d", bb);
+//    printf("char if 65 + 1:%c\n", 65);
+
+//    printf("address of a: %p, size of a: %u", &a, sizeof(a));
 
 //    printf("%lu\n",sizeof(int) );
 //    printf("This is for signed:\n");
--- src/lec3/typecasting.c
+++ src/lec3/typecasting.c
@@ -10,12 +10,16 @@ int main(void)
 {
     unsigned int a = 1000;
     signed int b = -1; // 2's complement format
-
+//
+    printf("signed b: %d\n", b);
     printf("unsigned b conversion: %u\n", b);
-    printf("unsigned b conversion: %u\n", UINT_MAX);
+    printf("unsigned -2 conversion: %u\n", -2);
+    printf("UINT_MAX conversion: %u\n", UINT_MAX);
 
-    //
-    if (a > b)
+    //compare two data types ; the signed get converted to unsigned
+    // don't compare sign and unsigned
+    // if we have to, typecast the unsigned to signed
+    if ((signed) a  > b)
         printf("a is more than b");
     else
         printf("a is less or equal than b");
