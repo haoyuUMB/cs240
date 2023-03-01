75abb5ad84144517897d9711591a2727b61b7970
haoyu <haoyu.wang001@umb.edu>
2023-03-01 15:32:10 -0500

    using library

---
+++ src/clients/using_lib/main.c
@@ -0,0 +1,9 @@
+//
+// Created by haoyu on 3/1/2023.
+//
+#include <stdio.h>
+#include <cs240/utils.h>
+
+int main() {
+    printf("Hello %d", max(2, 1));
+}
---
+++ src/lib/include/cs240/utils.h
@@ -0,0 +1,10 @@
+//
+// Created by haoyu on 3/1/2023.
+//
+
+#ifndef CS240_UTILS_H
+#define CS240_UTILS_H
+
+#define max(a, b) (a) >= (b) ? a : b
+
+#endif //CS240_UTILS_H
