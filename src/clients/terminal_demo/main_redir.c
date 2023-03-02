4a838cc749c0b9302eca28dbf1676be581a02520
haoyu <haoyu.wang001@umb.edu>
2023-03-02 14:42:48 -0500

    changes

--- src/clients/terminal_demo/main_redir.c
+++ src/clients/terminal_demo/main_redir.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-int main(int argc, char* argv[]){
-	printf("argc: %d: app name%s: 1st cla:%s\n", argc, argv[0], argv[1]);
-	return 0;
-}
-#include <stdio.h>
-int main(int argc, char* argv[]){
-	printf("argc: %d: app name%s: 1st cla:%s\n", argc, argv[0], argv[1]);
-	return 0;
+
+int main(int argc, char *argv[]) {
+    printf("argc: %d: app name%s: 1st cla:%s\n", argc, argv[0], argv[1]);
+    return 0;
 }
